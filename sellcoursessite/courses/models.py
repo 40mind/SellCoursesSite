67bccebb7996ff90dd3a1b3cb30e649a28f438e5
40mind <maxim.sorokoumov@yandex.ru>
2021-10-21 19:26:11 +0300

    Added models

--- sellcoursessite/courses/models.py
+++ sellcoursessite/courses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Direction(models.Model):
+    name = models.CharField(max_length=30, help_text="Введите название направления")
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=30, help_text="Введите название курса")
+    num = models.IntegerField(help_text="Введите количество занятий")
+    time = models.IntegerField(help_text="Введите время одного занятия")
+    days = models.CharField(max_length=100, help_text="Введите дни недели когда будут проходить занятия (через запятую)")
+    firstdate = models.DateField(help_text="Введите дату первого занятия")
+    lastdate = models.DateField(help_text="Введите дату последнего занятия")
+    price = models.IntegerField(help_text="Введите стоимость курса")
+    info = models.TextField(help_text="Введите подробную информацию про курс")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('course-detail', args=[str(self.name)])
+
